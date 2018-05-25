0fa57a533379252b1ac88cef6adfcc20f36dfd41
Shankar Kulumani <shanks.k@gmail.com>
2018-05-24 21:10:19 -0400

    Update build script

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -11,9 +11,6 @@ INSTALL_DIR="/usr/local/include"
 # HDF5_RELEASE_URL="https://support.hdfgroup.org/ftp/HDF5/current/src/hdf5-${HDF5_VER}.tar.gz"
 HDF5_RELEASE_URL="https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.2/src/hdf5-${HDF5_VER}.tar.gz"
 
-# This will download the latest eigen and install for the sy tem
-echo "Going to install the latest Eigen"
-
 # download  latest release of eigen
 if [[ ! "$TEMP_DIR" || ! -d "$TEMP_DIR" ]]; then
 	echo "Could not create temp dir"
@@ -34,31 +31,6 @@ mkdir ${EIGEN_VER}
 wget ${EIGEN_RELEASE_URL} -O ${TEMP_DIR}/${EIGEN_VER}.tar.gz
 tar -xvzf ${EIGEN_VER}.tar.gz -C ./${EIGEN_VER} --strip-components=1
 
-# move the eigen subdirectory to /usr/local/include
-if [[ -d "${INSTALL_DIR}/Eigen" ]]; then
-	echo "Eigen directory already  exists"
-	rm -rf "${INSTALL_DIR}/Eigen"
-fi
-
-# delete the unsupported directory
-if [[ -d "${INSTALL_DIR}/unsupported" ]]; then
-    echo "Eigen unsupported directory already exists"
-    rf -rf "${INSTALL_DIR}/Eigen"
-fi
-
-# echo "Now going to copy Eigen headers to /usr/include/local"
-# read -p "Press enter to continue"
-
-# # copy to /usr/local/include
-# echo "Now copying to ${INSTALL_DIR}/Eigen"
-# sudo mv ${EIGEN_VER}/Eigen ${INSTALL_DIR}
-
-# # copy to usr/local/include
-# echo "Now copying to ${INSTALL_DIR}/unsupported"
-# sudo mv ${EIGEN_VER}/unsupported ${INSTALL_DIR}
-
-# echo "Eigen ${EIGEN_VER} installed to ${INSTALL_DIR}/Eigen and ${INSTALL_DIR}/unsupported"
-
 echo "Going to install Eigen using CMake"
 cd ${EIGEN_VER}
 mkdir build
@@ -69,6 +41,7 @@ sudo make install
 echo "Eigen installed"
 
 echo "We're going to download HDF5 ${HDF5_VER} and install to ${INSTALL_DIR}"
+cd ${TEMP_DIR}
 mkdir ${HDF5_VER}
 wget ${HDF5_RELEASE_URL} -O ${TEMP_DIR}/${HDF5_VER}.tar.gz
 tar -xvzf ${HDF5_VER}.tar.gz -C ./${HDF5_VER} --strip-components=1
