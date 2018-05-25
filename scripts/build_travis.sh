aedc63670dc9e26f0021807d87498986962a40da
Shankar Kulumani <shanks.k@gmail.com>
2018-05-24 20:51:35 -0400

    Install eigen from source

--- scripts/build_travis.sh
+++ scripts/build_travis.sh
@@ -27,8 +27,48 @@ function cleanup {
 
 trap cleanup EXIT
 
+echo "We're going to download Eigen ${EIGEN_VER} and install to ${INSTALL_DIR}"
+cd ${TEMP_DIR}
+mkdir ${EIGEN_VER}
+wget ${EIGEN_RELEASE_URL} -O ${TEMP_DIR}/${EIGEN_VER}.tar.gz
+tar -xvzf ${EIGEN_VER}.tar.gz -C ./${EIGEN_VER} --strip-components=1
+
+# move the eigen subdirectory to /usr/local/include
+if [[ -d "${INSTALL_DIR}/Eigen" ]]; then
+	echo "Eigen directory already  exists"
+	rm -rf "${INSTALL_DIR}/Eigen"
+fi
+
+# delete the unsupported directory
+if [[ -d "${INSTALL_DIR}/unsupported" ]]; then
+    echo "Eigen unsupported directory already exists"
+    rf -rf "${INSTALL_DIR}/Eigen"
+fi
+
+# echo "Now going to copy Eigen headers to /usr/include/local"
+# read -p "Press enter to continue"
+
+# # copy to /usr/local/include
+# echo "Now copying to ${INSTALL_DIR}/Eigen"
+# sudo mv ${EIGEN_VER}/Eigen ${INSTALL_DIR}
+
+# # copy to usr/local/include
+# echo "Now copying to ${INSTALL_DIR}/unsupported"
+# sudo mv ${EIGEN_VER}/unsupported ${INSTALL_DIR}
+
+# echo "Eigen ${EIGEN_VER} installed to ${INSTALL_DIR}/Eigen and ${INSTALL_DIR}/unsupported"
+
+echo "Going to install Eigen using CMake"
+cd ${EIGEN_VER}
+mkdir build
+cd build
+cmake ..
+sudo make install
+
+echo "Eigen installed"
+
 sudo apt-get update
-sudo apt-get install -y libeigen3-dev libhdf5-dev libhdf5-serial-dev hdf5-tools hdf5-helpers
+sudo apt-get install -y libhdf5-dev libhdf5-serial-dev hdf5-tools hdf5-helpers
 
 echo "Now try to build the module"
 mkdir build 
