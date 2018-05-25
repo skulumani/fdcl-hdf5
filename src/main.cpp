bd0d56ff40525592c7b96e49922686310f674d2d
Shankar Kulumani <shanks.k@gmail.com>
2018-05-24 20:04:01 -0400

    Update example

--- src/main.cpp
+++ src/main.cpp
@@ -25,22 +25,13 @@ void read_data() {
     Eigen::MatrixXd matrix;
 
     hf.read("dataset_name", matrix);
-
+    
+    std::cout << "Matrix read: " << std::endl;
     std::cout << matrix << std::endl;
 }
 
 int main() {
-
-    // create a new HDF5 file, overwriting if it already exists
-    std::shared_ptr<HDF5::File> hf_file_ptr = std::make_shared<HDF5::File>("/tmp/test.hdf5", HDF5::File::ReadWrite);
-    Eigen::MatrixXd mat(1, 3), mat_load(1, 3);
-    mat = Eigen::MatrixXd::Random(1, 3);
-    hf_file_ptr->write("matrix", mat);
-    
-    // close the file by reset
-    hf_file_ptr.reset(new HDF5::File("/tmp/test.hdf5", HDF5::File::ReadOnly));
-
-    // now test dataset constructor
-    HDF5::DataSet hf_dataset(hf_file_ptr.get(), "matrix");
+    write_data();
+    read_data();
     return 0;
 }
