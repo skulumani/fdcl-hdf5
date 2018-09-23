39b22339055505f491759f9cdcfbb26d042e2964
Shankar Kulumani <shanks.k@gmail.com>
2018-09-23 16:59:30 -0400

    Add test for 1x3 integers

--- tests/test_eigen_sizes.cpp
+++ tests/test_eigen_sizes.cpp
@@ -158,6 +158,15 @@ TEST(TestFDCLUAVFile, Matrix1x3Double) {
     ASSERT_TRUE(mat_read.isApprox(mat));
 }
 
+TEST(TestFDCLUAVFile, Matrix1x3Int) {
+    HDF5::File hf_file("/tmp/test.hdf5", HDF5::File::Truncate);
+    Eigen::Matrix<int, 1, 3> mat, mat_read;
+    mat << 1, 2, 3;
+    hf_file.write("matrix", mat);
+    hf_file.read("matrix", mat_read);
+    ASSERT_TRUE(mat_read.isApprox(mat));
+}
+
 TEST(TestFDCLUAVFile, Eigen3by3) {
     HDF5::File hf_file("/tmp/test.hdf5", HDF5::File::Truncate);
     Eigen::Matrix<double, 3, 3> mat, mat_read;
